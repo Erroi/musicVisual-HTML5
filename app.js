14d194797683e86b48ee8325eb529c17b76ca939
yangqian <973860441@qq.com>
2016-11-30 14:38:59 +0800

    index

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var ejs = require('ejs');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, './'));
+app.set('views', path.join(__dirname, 'views'));
 app.engine('html',ejs.__express);
 app.set('view engine', 'html');
 
